88c9dc324a8a9570321f3efb091d1662b9e19738
aparinelli <alejoparinelli@gmail.com>
2022-12-21 00:39:20 -0300

    imma hop

--- run.py
+++ run.py
@@ -6,9 +6,6 @@ DATABASE_PATH = os.path.abspath(os.getcwd() + '/app/database.db')
 
 
 if __name__ == '__main__':
-    if os.path.exists(DATABASE_PATH):
-        os.remove(DATABASE_PATH)
-
     db.create_all()
     flask_app.run(debug=True)
     
