dc4392db532f58c707c799a731e5d15d0a351465
aparinelli <alejoparinelli@gmail.com>
2022-12-21 00:27:47 -0300

    add db create all on run

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
-from app import create_app
+import os
+from app import create_app, db
 
 flask_app = create_app()
 
 if __name__ == '__main__':
+    with flask_app.app_context():
+        db.create_all()
+
     flask_app.run(debug=True)
     
